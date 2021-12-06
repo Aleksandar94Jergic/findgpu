a9a491107057493fbaafc467b72c0a2fd943e8d0
Aleksandar94Jergic <acojergic@gmail.com>
2021-12-06 13:58:43 +0100

    Update findgpu.sql
    shop broj 2

--- findgpu.sql
+++ findgpu.sql
@@ -43,3 +43,4 @@ alter table shop add foreign key (delivery) references delivery(code);
 
 insert into shop (code,name,address,price,quantity,worktime,delivery,email),
 values(null,'Links','Avenija Dubrovnik 14 10000 Zagreb','7999,99','15','Monday-Friday 08:00AM-08:00PM','Monday-Friday 09:00AM-05:00PM','ambienta@links.hr'),
+values(null,'Ipon','Francia út. 40/b Budapest','10999,99','20','Monday-Friday 10:00AM-04:00PM','Monday-Friday 09:00AM-05:00PM','info@iponcomp.hr'),
